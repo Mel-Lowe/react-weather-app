b497d381ef29f280cbdc1e854e1f3b36ec9bc136
Melowe <51891075+Mel-Lowe@users.noreply.github.com>
2023-04-26 16:50:47 +0100

    Added GitHub repo link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://playful-lebkuchen-8def1d.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mel Lowe
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Mel-Lowe/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+      </footer>
     </div>
   );
 }
