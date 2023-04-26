cd7fa3fcfbed1ddc90f5d0bd1121947fc3efbbea
Melowe <51891075+Mel-Lowe@users.noreply.github.com>
2023-04-26 18:44:47 +0100

    Styled weather data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://playful-lebkuchen-8def1d.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mel Lowe
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Mel-Lowe/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://playful-lebkuchen-8def1d.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mel Lowe
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Mel-Lowe/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
