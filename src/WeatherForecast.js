7981a96b2910beeaf3a3532398e422262d0b16f4
Melowe <51891075+Mel-Lowe@users.noreply.github.com>
2023-04-27 23:30:32 +0100

    built 5 day forecast with .map

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,9 +17,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay forecastDay={forecastData[0]} />
-          </div>
+          {forecastData.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay forecastDay={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
