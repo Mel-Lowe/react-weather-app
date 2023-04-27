092d98a96491bd47b38679f520a55f44506441ed
Melowe <51891075+Mel-Lowe@users.noreply.github.com>
2023-04-28 00:08:33 +0100

    updated Bootstrap button

--- src/Weather.js
+++ src/Weather.js
@@ -55,7 +55,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-success w-100"
+                className="btn btn-dark w-100"
               />
             </div>
           </div>
